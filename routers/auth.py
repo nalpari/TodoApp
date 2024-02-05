60c98d709773e74496e57d935f6d336114c6e4e9
sangwook.yoo <sangwook.yoo@cj.net>
2024-02-05 18:02:08 +0900

    Add user create

--- routers/auth.py
+++ routers/auth.py
@@ -3,11 +3,16 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from starlette import status
 
 from database import SessionLocal
 from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CreateUserRequest(BaseModel):
@@ -47,16 +52,19 @@ async def get_user():
     return {"message": "Hello World"}
 
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest, db: db_dependency):
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True,
     )
 
+    db.add(create_user_model)
+    db.commit()
+    db.refresh(create_user_model)
     return create_user_model
