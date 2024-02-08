d6bad1899e1880f61b8c2e8d41e1949783319966
sangwook.yoo <sangwook.yoo@cj.net>
2024-02-08 09:12:04 +0900

    refactor: Add TDD syntax

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
-
-import models
+from models import Base
 from database import engine
 from routers import auth, todos, admin, users
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
---
+++ test/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+import main
+from fastapi import status
+
+
+client = TestClient(main.app)
+
+
+def test_return_health_check():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "ok"}
